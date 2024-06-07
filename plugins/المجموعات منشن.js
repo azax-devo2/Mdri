7a27642664c48f1000b70d3c02d08e5ea1fdb99e
Xshm007 <jjuhj6777@gmail.com>
2024-06-07 20:58:19 +0300

    Update and rename gc-tagall.js to المجموعات منشن.js

--- plugins/المجموعات منشن.js
+++ plugins/المجموعات منشن.js
@@ -15,14 +15,14 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   const oi = `${tradutor.texto1[0]} ${pesan}`;
   let teks = `${tradutor.texto1[1]}  ${oi}\n\n${tradutor.texto1[2]}\n`;
   for (const mem of participants) {
-    teks += `┣➥ @${mem.id.split('@')[0]}\n`;
+    teks += `☬↫ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* 𝐁𝐲 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += `> *© 𝑫𝑬𝑨𝑫𝑷𝑶𝑶𝑳...*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
 handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
+handler.command = /^(منشن)$/i;
 handler.admin = true;
 handler.group = true;
 export default handler;
