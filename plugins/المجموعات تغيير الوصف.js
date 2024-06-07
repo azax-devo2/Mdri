d388f866ad0bdb7498ce3d31e8a90e2a99398b21
Xshm007 <jjuhj6777@gmail.com>
2024-06-08 01:02:21 +0300

    Update and rename gc-setdesc.js to المجموعات تغيير الوصف.js

--- plugins/المجموعات تغيير الوصف.js
+++ plugins/المجموعات تغيير الوصف.js
@@ -11,7 +11,7 @@ const handler = async (m, {conn, args}) => {
 };
 handler.help = ['Setdesc <text>'];
 handler.tags = ['group'];
-handler.command = /^setdesk|setdesc$/i;
+handler.command = /^تغيير-الوصف$/i;
 handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
