4aed621b222b575b2b7447f61cd6c17d8486f83b
Xshm007 <jjuhj6777@gmail.com>
2024-06-08 02:20:48 +0300

    Update المجموعات  طرد.js

--- plugins/المجموعات  طرد.js
+++ plugins/المجموعات  طرد.js
@@ -1,7 +1,7 @@
 
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 
-let kickte = `*قـم بالـرد على رسـالة الشخـص الذي تريد طرده او اشـر اليـه مثال \n.طرد ${taguser}*`
+let kickte = `*قـم بالـرد على رسـالة الشخـص الذي تريد طرده او اشـر اليـه*`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
