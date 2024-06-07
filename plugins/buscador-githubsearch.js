4b1817592967ae2832cf44300366cf4cd6b2416d
Xshm007 <jjuhj6777@gmail.com>
2024-06-07 16:16:22 +0100

    Update buscador-githubsearch.js

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -33,7 +33,7 @@ ${repo.description ? `📝 ${tradutor.texto1[4]}\n${repo.description}` : ''}
 };
 handler.help = ['githubs'];
 handler.tags = ['buscadores'];
-handler.command = /^(ghs|githubs|githubs|githubsearch|gits|gitsearch)$/i;
+handler.command = /^(غيتهاب|githubs|githubs|githubsearch|gits|gitsearch)$/i;
 export default handler;
 
 function formatDate(n, locale = 'es') {
