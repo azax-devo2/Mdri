bbf4ac93d85b5dc3f831f069f8847be9c7488c07
Xshm007 <jjuhj6777@gmail.com>
2024-06-08 01:30:56 +0300

    Update and rename gc-setwelcome.js to المجموعات تغيير الترحيب.js

--- plugins/المجموعات تغيير الترحيب.js
+++ plugins/المجموعات تغيير الترحيب.js
@@ -13,6 +13,6 @@ const handler = async (m, {conn, text, isROwner, isOwner}) => {
 };
 handler.help = ['setwelcome <text>'];
 handler.tags = ['group'];
-handler.command = ['setwelcome'];
+handler.command = ['تغيير-الترحيب'];
 handler.admin = true;
 export default handler;
