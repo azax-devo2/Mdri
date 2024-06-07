3f18de545c743aa3f4066d93ba1945ccf013a4ce
Xshm007 <jjuhj6777@gmail.com>
2024-06-07 15:23:32 +0100

    Update audio-efectos.js

--- plugins/audio-efectos.js
+++ plugins/audio-efectos.js
@@ -13,18 +13,18 @@ const handler = async (m, {conn, args, __dirname, usedPrefix, command}) => {
     const q = m.quoted ? m.quoted : m;
     const mime = ((m.quoted ? m.quoted : m.msg).mimetype || '');
     let set;
-    if (/bass/.test(command)) set = '-af equalizer=f=94:width_type=o:width=2:g=30';
-    if (/blown/.test(command)) set = '-af acrusher=.1:1:64:0:log';
-    if (/deep/.test(command)) set = '-af atempo=4/4,asetrate=44500*2/3';
-    if (/earrape/.test(command)) set = '-af volume=12';
-    if (/fast/.test(command)) set = '-filter:a "atempo=1.63,asetrate=44100"';
-    if (/fat/.test(command)) set = '-filter:a "atempo=1.6,asetrate=22100"';
-    if (/nightcore/.test(command)) set = '-filter:a atempo=1.06,asetrate=44100*1.25';
-    if (/reverse/.test(command)) set = '-filter_complex "areverse"';
-    if (/robot/.test(command)) set = '-filter_complex "afftfilt=real=\'hypot(re,im)*sin(0)\':imag=\'hypot(re,im)*cos(0)\':win_size=512:overlap=0.75"';
-    if (/slow/.test(command)) set = '-filter:a "atempo=0.7,asetrate=44100"';
-    if (/smooth/.test(command)) set = '-filter:v "minterpolate=\'mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=120\'"';
-    if (/tupai|squirrel|chipmunk/.test(command)) set = '-filter:a "atempo=0.5,asetrate=65100"';
+    if (/صاخب/.test(command)) set = '-af equalizer=f=94:width_type=o:width=2:g=30';
+    if (/مدري/.test(command)) set = '-af acrusher=.1:1:64:0:log';
+    if (/عميق/.test(command)) set = '-af atempo=4/4,asetrate=44500*2/3';
+    if (/مزعج/.test(command)) set = '-af volume=12';
+    if (/سريع/.test(command)) set = '-filter:a "atempo=1.63,asetrate=44100"';
+    if (/ثخين/.test(command)) set = '-filter:a "atempo=1.6,asetrate=22100"';
+    if (/ليلي/.test(command)) set = '-filter:a atempo=1.06,asetrate=44100*1.25';
+    if (/تذبذب/.test(command)) set = '-filter_complex "areverse"';
+    if (/روبو/.test(command)) set = '-filter_complex "afftfilt=real=\'hypot(re,im)*sin(0)\':imag=\'hypot(re,im)*cos(0)\':win_size=512:overlap=0.75"';
+    if (/بطيئ/.test(command)) set = '-filter:a "atempo=0.7,asetrate=44100"';
+    if (/سلس/.test(command)) set = '-filter:v "minterpolate=\'mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=120\'"';
+    if (/سنجاب|squirrel|chipmunk/.test(command)) set = '-filter:a "atempo=0.5,asetrate=65100"';
     if (/audio/.test(mime)) {
       const ran = getRandom('.mp3');
       const filename = join(__dirname, '../tmp/' + ran);
@@ -45,7 +45,7 @@ const handler = async (m, {conn, args, __dirname, usedPrefix, command}) => {
 };
 handler.help = ['bass', 'blown', 'deep', 'earrape', 'fast', 'fat', 'nightcore', 'reverse', 'robot', 'slow', 'smooth', 'tupai'].map((v) => v + ' [vn]');
 handler.tags = ['audio'];
-handler.command = /^(bass|blown|deep|earrape|fas?t|nightcore|reverse|robot|slow|smooth|tupai|squirrel|chipmunk)$/i;
+handler.command = /^(صاخب|مدري|عميق|مزعج|سريع|ليلي|تذبذب|روبو|بطيئ|سلس|سنجاب|squirrel|chipmunk)$/i;
 export default handler;
 
 const getRandom = (ext) => {
