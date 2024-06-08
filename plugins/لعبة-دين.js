b8a117bb87f67211dc00c37313fbd2b65d1dfdf5
Xshm007 <jjuhj6777@gmail.com>
2024-06-08 12:16:40 +0100

    Create لعبة-دين.js

---
+++ plugins/لعبة-دين.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+let timeout = 60000;
+let poin = 500;
+
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tekateki = conn.tekateki ? conn.tekateki : {};
+    let id = m.chat;
+    if (id in conn.tekateki) {
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tekateki[id][0]);
+        throw false;
+    }
+
+    // URL لملف Gist الذي يحتوي على الأسئلة
+    let gistUrl = 'https://gist.githubusercontent.com/SHIKA7777/6829198a706c33b41d013ec11fe25a3e/raw/fef2cebf1ea43dd7341c9cc07b620af114f53d75/gistfile1.txt';
+
+    try {
+        let response = await axios.get(gistUrl);
+        let tekateki = response.data;
+        let json = tekateki[Math.floor(Math.random() * tekateki.length)];
+        let _clue = json.response;
+        let clue = _clue.replace(/[A-Za-z]/g, '_');
+        let caption = `
+ⷮ *${json.question}*
+
+*الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)}*
+*الـجـائـزة💰↞ ${poin} نقاط*
+*KANEKI BOT*
+`.trim();
+        let image = 'https://telegra.ph/file/f5c021416da60ccd37f00.jpg'; // رابط الصورة الجديدة
+        conn.tekateki[id] = [
+            await conn.sendFile(m.chat, image, 'image.jpg', caption, m),
+            json, poin,
+            setTimeout(async () => {
+                if (conn.tekateki[id]) await conn.reply(m.chat, `*⌛انتهي الوقت⌛*\n *الاجـابـة✅ ${json.response}*`, conn.tekateki[id][0]);
+                delete conn.tekateki[id];
+            }, timeout)
+        ];
+    } catch (error) {
+        console.error('Error fetching Gist data:', error);
+        conn.reply(m.chat, '❐┃حدث خطأ أثناء جلب الأسئلة من الملف┃❌', m);
+    }
+};
+
+handler.help = ['acertijo'];
+handler.tags = ['game'];
+handler.command = /^(دين)$/i;
+
+export default handler;
