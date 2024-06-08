c0dd5250e4ffac73b8b38f908373c0e44e0e858d
Xshm007 <jjuhj6777@gmail.com>
2024-06-08 01:53:25 +0100

    Update herramientas-topdf.js

--- plugins/herramientas-topdf.js
+++ plugins/herramientas-topdf.js
@@ -15,5 +15,5 @@ const handler = async (m, {conn, text, usedPrefix, command, isOwner}) => {
   const docname = text ? text : m.pushName || 'documento';
   conn.sendFile(m.chat, `http://api.lolhuman.xyz/api/convert/imgtopdf?apikey=${lolkeysapi}&img=${url}`, docname + '.pdf', '', m, false, {asDocument: true});
 };
-handler.command = /^topdf$/i;
+handler.command = /^لملف$/i;
 export default handler;
