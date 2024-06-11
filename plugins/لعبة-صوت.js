b26fe077915afd6b9b446d91dccbdb996bce71d9
Xshm007 <jjuhj6777@gmail.com>
2024-06-11 01:25:19 +0100

    Update لعبة-صوت.js

--- plugins/لعبة-صوت.js
+++ plugins/لعبة-صوت.js
@@ -6,20 +6,17 @@ const handler = async (m, {conn, usedPrefix}) => {
   conn.tebaklaguo = conn.tebaklaguo ? conn.tebaklaguo : {};
   const id = m.chat;
   if (id in conn.tebaklaguo) {
-    conn.reply(m.chat, '*⚠️〔هناك سؤال بالفعل〕⚠️*', conn.tebaklaguo[id][0]);
+    conn.reply(m.chat, '*هناك سؤال قائم💫*', conn.tebaklaguo[id][0]);
     throw false;
   } // 5LTV57azwaid7dXfz5fzJu
   const res = await fetchJson(`https://gist.githubusercontent.com/Stark197/e6eb5b13b875f2178cc6b6a5c18ee9ac/raw/7e4d026b19799908d3aaf1d728b1d23634999d48/gistfile1.txt`);
   const json = res[Math.floor(Math.random() * res.length)];
-  const caption = `*~⊹‏⊱≼━━━⌬〔📜〕⌬━━━≽⊰⊹~*
-*${command.toUpperCase()}*
-*☬↫╎السـؤال ✍🏻⇜『تعرف على الشخصية من صوتها』*
-  *☬↫╎الـوقـت⏱️↫ ${(timeout / 1000).toFixed(2)} ┇*
-  *استخدم .انسحب للأنسحاب*
-  *☬↫╎الـجـائزة🪙↫ ${poin} نقاط┇*
-    *☬↫╎مــلاحظة📁↫ قم بالرد على هذه الرسالة┇*
-*~⊹‏⊱≼━━━⌬〔📜〕⌬━━━≽⊰⊹~*
-> ©𝐷𝐸𝐴𝐷𝑃𝑂𝑂𝐿 ↯`.trim();
+  const caption = `*~⊹‏⊱≼━━━⌬〔📜〕⌬━━━≽⊰⊹~*\n*صوت من ؟؟*\n
+  *☬↫╎السـؤال ✍🏻⇜『اعرف الشخصية من صوتها』*
+  *ا☬↫╎الـوقـت⏱️↫*${(timeout / 1000).toFixed(2)}* *ثانية*
+   *☬↫╎الـجـائزة🪙↫* *${poin}* *خبرة*
+   *☬↫╎مـلاحظة⚠️قم برد على هذه الرسالة عند الاجابة』*
+*~⊹‏⊱≼━━━⌬〔📜〕⌬━━━≽⊰⊹~*`.trim();
   conn.tebaklaguo[id] = [
     await m.reply(caption),
     json, poin,
@@ -28,12 +25,24 @@ const handler = async (m, {conn, usedPrefix}) => {
       delete conn.tebaklaguo[id];
     }, timeout),
   ];
+  const isAudioReply = m.quoted && m.quoted.isVoice;
+  const isTextReply = m.quoted && m.quoted.text;
+  const isDirectAnswer = m.text && !m.isGroup;
+  if (isAudioReply || isTextReply || isDirectAnswer) {
+    const answer = isAudioReply ? m.quoted : m;
+    const text = isTextReply ? answer.quoted.text : isDirectAnswer ? m.text : '';
+    if (text.trim().toLowerCase() == json.jawaban.trim().toLowerCase()) {
+      clearTimeout(conn.tebaklaguo[id][3]);
+      conn.reply(m.chat, `*~⊹‏⊱≼━━━⌬〔📜〕⌬━━━≽⊰⊹~*\n*لإجابة صحيحة!✅*\n*🧛الفائز:* *@${m.sender.split`@`[0]}*\n*✨الجائزة: ${poin} خبرة*\n**~⊹‏⊱≼━━━⌬〔📜〕⌬━━━≽⊰⊹~*`, conn.tebaklaguo[id][0]);
+      delete  conn.tebaklaguo[id];
+    }
+  }
   const aa = await conn.sendMessage(m.chat, {audio: {url: json.link_song}, fileName: `error.mp3`, mimetype: 'audio/mpeg'}, {quoted: m});
-  if (!aa) return conn.sendFile(m.chat, json.jawaban, 'By Azax.mp3', '', m);
+  if (!aa) return conn.sendFile(m.chat, json.jawaban, 'coba-lagi.mp3', '', m);
 };
 handler.help = ['tebaklaguo'];
 handler.tags = ['game'];
-handler.command = /^اوست$/i;
+handler.command = /^اوست|canción$/i;
 export default handler;
 async function fetchJson(url, options) {
   try {
